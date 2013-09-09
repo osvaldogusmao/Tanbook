f5d1946f545e14f901166fc1f999e2f64aef7cea
Mauricio Andrade <mauricio_andrade@outlook.com>
2013-09-09 20:37:54 -0300

    Atualização do banco com campos que estava faltando

--- banco/crud_tanbook.sql
+++ banco/crud_tanbook.sql
@@ -2,42 +2,46 @@ SET @OLD_UNIQUE_CHECKS=@@UNIQUE_CHECKS, UNIQUE_CHECKS=0;
 SET @OLD_FOREIGN_KEY_CHECKS=@@FOREIGN_KEY_CHECKS, FOREIGN_KEY_CHECKS=0;
 SET @OLD_SQL_MODE=@@SQL_MODE, SQL_MODE='TRADITIONAL,ALLOW_INVALID_DATES';
 
-CREATE SCHEMA IF NOT EXISTS `crud_tanbook` DEFAULT CHARACTER SET utf8 COLLATE utf8_general_ci ;
+CREATE SCHEMA IF NOT EXISTS `crud_tanbook` DEFAULT CHARACTER SET utf8 ;
 USE `crud_tanbook` ;
 
 -- -----------------------------------------------------
 -- Table `crud_tanbook`.`usuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `crud_tanbook`.`usuario` (
-  `codigo` INT NOT NULL AUTO_INCREMENT,
-  `nome` VARCHAR(45) NULL,
-  `login` VARCHAR(25) NULL,
-  `senha` VARCHAR(25) NULL,
-  `email` VARCHAR(30) NULL,
-  `datanascimento` DATE NULL,
-  `nick name` VARCHAR(45) NULL,
-  `facebook` VARCHAR(55) NULL,
-  `avatar` MEDIUMBLOB NULL,
-  PRIMARY KEY (`codigo`))
-ENGINE = InnoDB;
+CREATE  TABLE IF NOT EXISTS `crud_tanbook`.`usuario` (
+  `codigo` INT(11) NOT NULL AUTO_INCREMENT ,
+  `nome_completo` VARCHAR(45) NULL DEFAULT NULL ,
+  `login` VARCHAR(25) NULL DEFAULT NULL ,
+  `senha` VARCHAR(25) NULL DEFAULT NULL ,
+  `email` VARCHAR(30) NULL DEFAULT NULL ,
+  `data_nascimento` DATE NULL DEFAULT NULL ,
+  `apelido` VARCHAR(45) NULL DEFAULT NULL ,
+  `url_facebook` VARCHAR(55) NULL DEFAULT NULL ,
+  `avatar` MEDIUMBLOB NULL DEFAULT NULL ,
+  `sexo` VARCHAR(20) NULL ,
+  `chave_validacao` VARCHAR(45) NULL ,
+  PRIMARY KEY (`codigo`) )
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
 
 
 -- -----------------------------------------------------
 -- Table `crud_tanbook`.`tipousuario`
 -- -----------------------------------------------------
-CREATE TABLE IF NOT EXISTS `crud_tanbook`.`tipousuario` (
-  `grupo` INT NOT NULL,
-  `nivelacesso` INT NULL,
-  `sexo` VARCHAR(20) NULL,
-  `usuario_codigo` INT NOT NULL,
-  PRIMARY KEY (`grupo`, `usuario_codigo`),
-  INDEX `fk_tipousuario_usuario_idx` (`usuario_codigo` ASC),
+CREATE  TABLE IF NOT EXISTS `crud_tanbook`.`tipousuario` (
+  `nivelacesso` INT(11) NULL DEFAULT NULL ,
+  `usuario_codigo` INT(11) NOT NULL ,
+  PRIMARY KEY (`usuario_codigo`) ,
+  INDEX `fk_tipousuario_usuario_idx` (`usuario_codigo` ASC) ,
   CONSTRAINT `fk_tipousuario_usuario`
-    FOREIGN KEY (`usuario_codigo`)
-    REFERENCES `crud_tanbook`.`usuario` (`codigo`)
+    FOREIGN KEY (`usuario_codigo` )
+    REFERENCES `crud_tanbook`.`usuario` (`codigo` )
     ON DELETE NO ACTION
     ON UPDATE NO ACTION)
-ENGINE = InnoDB;
+ENGINE = InnoDB
+DEFAULT CHARACTER SET = utf8;
+
+USE `crud_tanbook` ;
 
 
 SET SQL_MODE=@OLD_SQL_MODE;
