5cdbe1cb3800b204cd51ef828d2062eeef8b18c9
José Osvaldo Gusmão <osvaldogusmao@yahoo.com.br>
2013-09-09 21:40:37 -0300

    Add comentários

--- test/testController.php
+++ test/testController.php
@@ -11,6 +11,7 @@ include_once '../functions/crud.class.php';
 class Test extends crud {
 
 	public function __construct(){
+		//Deve obrigatoriamente passar um parametro, que deve ser o mesmo nome da tabela no BD
 		parent::__construct("teste");
 	}
 
