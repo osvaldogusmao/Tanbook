439a41d8d505c16a88b8771c911412b17024244c
Lukas Robeto <lukas.roberto@yahoo.com.br>
2013-09-15 19:35:48 -0300

    Alteracoes feitas nas paginas lispa.php e lista.usuario.controller.php

--- controller/lista.usuario.controller.class.php
+++ controller/lista.usuario.controller.class.php
@@ -1,5 +1,12 @@
 ﻿<?php
 
+/*
+	Descrição do arquivo: Controller responsavel pelas consultas para listagem de usuarios na tela lista.php
+	@autor - Lukas Roberto
+	@data de criação - 07/09/2013
+	@arquivo - lista.usuario.controller.php
+*/
+
 include_once "../../functions/crud.class.php";
 include_once "../../functions/connection.class.php";
 
@@ -14,8 +21,11 @@ class usuarioControl extends crud{
 	public function listarUsuario($where = NULL){	
 		
 		if ($where){
-			$select = $this->execute_query("SELECT * FROM ".$this->getTabela()." WHERE ".$where);
+			//$select = $this->execute_query("SELECT * FROM usuario, tipodeusuario WHERE usuario.tipoDeUsuario_id = tipodeusuario.id and usuario.id =".$where);
+			$select = $this->execute_query("SELECT * FROM ".$this->getTabela()." WHERE id =".$where);
 		}else{
+			//$select = $this->execute_query("SELECT * FROM usuario, tipodeusuario, grupoDeUsuario WHERE usuario.grupoDeUsuario_id = grupoDeUsuario.id  and usuario.tipoDeUsuario_id = tipodeusuario.id");
+
 			$select = $this->execute_query("SELECT * FROM ".$this->getTabela());
 	  	}
 		
@@ -29,6 +39,10 @@ class usuarioControl extends crud{
 		}else{
 			return false;
 		}
+	}
+			public function tipoUsuario($where){	
+
+			return $select = $this->execute_query("SELECT * FROM tipodeusuario WHERE id =".$where);
 
 	}  	
 
--- view/usuario/lista.php
+++ view/usuario/lista.php
@@ -1,4 +1,13 @@
 ﻿<?php
+
+/*
+	Descrição do arquivo: Esta é a pagina responsavel pos listar todos os usuarios
+	@autor - Lukas Roberto
+	@data de criação - 07/09/2013
+	@arquivo - lista.php
+
+*/
+
 include_once "../../controller/lista.usuario.controller.class.php";
 
 ?>
@@ -15,46 +24,74 @@ include_once "../../controller/lista.usuario.controller.class.php";
   <header> LOGO AQUI </header>
   <nav>MENU</nav>
   <section id="conteudo">
-    <?php
+    <p>
+      <?php
+	  $idUsuario = NULL;
+	  if(isset($_POST["idUsuario"])){
+		$idUsuario = $_POST["idUsuario"];
+		}
 
-$usuario_controller = new usuarioControl();
-$resultados = $usuario_controller->listarUsuario();
+	$usuario_controller = new usuarioControl();
+	$resultados = $usuario_controller->listarUsuario($idUsuario);	
 
-?>
-        <?php 
+	?>
+      <?php 
 			//verifica se existem usuarios cadastrados, se existirem lista os usuarios
 			if ($resultados){
 		?>
+    <div id="filtro">
+      <form name="form1" method="post" action="lista.php">
+        <label for="idUsuario"></label>
+        <input type="text" name="idUsuario" id="idUsuario">
+        <input type="submit" name="button" id="button" value="Buscar">
+      </form>
+    </div>
+    </p>
+    <p>&nbsp;</p>
     <table>
       <thead>
         <tr>
-          <th>Código</th>
-          <th>Nome Completo</th>
+          <th>Avatar</th>
+          <th>Apelido</th>
+          <th>Nome</th>
+          <th>E-mail</th>
+          <th>Sexo</th>
+          <th>Data de Nascimento</th>
+          <th>Facebook</th>
+          <th>Tipo de Usuário</th>
+          <th>Grupo do Usuário</th>
           <th>Alterar</th>
           <th>Deletar</th>
         </tr>
       </thead>
       <tbody>
-		<?php
-				while($campos = mysql_fetch_array($resultados)): 
+        <?php
+				while($campos = mysql_fetch_array($resultados)):
 		?>
         <tr>
-          <td><?php echo $campos["id"] ?></td>
+          <td><?php echo $campos["avatar"] ?></td>
+          <td><?php echo $campos["apelido"] ?></td>
           <td><?php echo $campos["nome"] ?></td>
-          <td><a href="editar.php?codigo=<?php echo $campos["id"] ?>" class="tbListaUsuario">Editar</a></td>
-          <td><a href="excluir.php?codigo=<?php echo $campos["id"] ?>" class="tbListaUsuario">Editar</a></td>
+          <td><?php echo $campos["email"] ?></td>
+          <td><?php echo $campos["sexo"] ?></td>
+          <td><?php echo $campos["dataDeNascimento"] ?></td>
+          <td><?php echo $campos["urlFacebook"] ?></td>
+          <td><?php echo $campos["tipoDeUsuario_id"] ?></td>
+          <td><?php echo $campos["grupoDeUsuario_id"] ?></td>
+          <td><a href="editar.php?id=<?php echo $campos["id"] ?>" class="tbListaUsuario">Editar</a></td>
+          <td><a href="excluir.php?id=<?php echo $campos["id"] ?>" class="tbListaUsuario">Excluir</a></td>
         </tr>
         <?php
 		  		endwhile;
 		?>
       </tbody>
     </table>
-    	<?php		
+    <?php		
                 //se não existir usuarios cadastrados, exibe menssagem	
 	  		}else{
 		?>
-        <div class="alerta"> Sem Usuários Cadastrados </div>
-        <?php
+    <div class="alerta"> Sem Usuários Cadastrados </div>
+    <?php
 				}
  		  ?>
   </section>
