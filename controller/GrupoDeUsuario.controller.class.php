dfc00e4b89217bcc053300d0f32b84169ca31e59
Rafael Freitas <rsfreitas90@gmail.com>
2013-09-19 13:23:25 -0300

    1 sprint

---
+++ controller/GrupoDeUsuario.controller.class.php
@@ -0,0 +1,87 @@
+<?php
+
+/*
+
+  Controlador da classe GrupoDeUsuario- (controlar o CRUD do GrupoDeUsuario)
+
+  @author Rafael Freitas
+
+  @date 17/09/2013 – Data da criação do arquivo
+
+  @nameFile GrupoDeUsuarioController.class.php (GrupoDeUsuarioController)
+
+*/
+
+include_once "../../functions/crud.class.php";
+
+class GrupoDeUsuarioController extends crud{
+	  
+	//Atributos
+	
+	private $sql_select = "";
+
+
+	// Método construtor
+	
+	public function __construct(){
+		parent::__construct("GrupoDeUsuario");
+	}
+  		
+
+	// Método de listagem
+
+	public function listarGrupoDeUsuario($where = NULL){
+		
+		if ($where){
+			$this->sql_select = "SELECT * FROM ".$this->getTabela()." WHERE ".$where;
+		}else{
+			$this->sql_select = "SELECT * FROM ".$this->getTabela();
+	  	}
+		
+		$con = new Connection();
+		$con->openConnection();
+
+	  	$sel = mysql_query($this->sql_select);
+		$regs = mysql_num_rows($sel);
+
+		
+		
+	  	if ($regs > 0){
+		
+			// Início
+			
+			$estrutura = "<table><tr><th>Código</th><th>Nome</th><th>Apelido</th><th>Quantidade de Licensa</th><th>Validade de Licensa</th><th>Chave da Licensa</th><th>Status</th><th>Data de Cadastro</th><th>Editar</th><th>Excluir</th></tr>";
+			
+    		while($campo = mysql_fetch_array($sel)){ // laço de repetiçao que vai trazer todos os resultados da consulta
+                    $estrutura .= 	"
+									<tr>
+										<td>".$campo['codigo']."</td>
+										<td>".$campo['nome']."</td>
+										<td>".$campo['apelido']."</td>
+										<td>".$campo['quantidadeDeLicensa']."</td>
+										<td>".$campo['validadeDaLicensa']."</td>
+										<td>".$campo['chaveDaLicensa']."</td>
+										<td>".$campo['status']."</td>
+										<td>".$campo['dataDeCadastro']."</td>
+										<td><a href=\"\" class='tbListaUsuario'\">Editar</a></td>
+										<td><a href=\"\" class='tbListaUsuario'\" >Excluir</a></td>
+									</tr>
+									";
+			}
+			
+			$estrutura .= "</table>";
+			$con->closeConnection();
+			// Fim
+			
+			return $estrutura;
+
+		}else{
+			return "Nenhum registro encontrado!";
+		}
+
+
+	}  	
+
+}  		
+  	
+?>
---
+++ view/grupodeusuario/lista.php
@@ -0,0 +1,51 @@
+<?php
+/*
+
+  View ListaGrupoDeUsuario (listar todos os grupos de usuarios cadastrados no banco.)
+  @author Rafael Freitas
+  @date 17/09/2013 – Data da criação do arquivo
+  @nameFile lista.GrupoDeUsuario.class.php (ListaGrupoDeUsuario)
+
+*/
+include_once "../../functions/connection.class.php";
+include_once "../../controller/GrupoDeUsuario.controller.class.php";
+?>
+
+<!DOCTYPE html>
+<html>
+<head>
+<meta charset="utf-8">
+<title>Projeto Tanbook</title>
+<link href="../css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section id="wrapper">
+  <header> LOGO AQUI </header>
+  <nav>MENU</nav>
+  <section id="conteudo">
+    <?php
+// Conectando no banco de dados
+
+$con = new Connection();
+$con->openConnection();
+
+// Criando a instância da classe e setando a tabela de referência
+
+$GrupoDeUsuarioController = new GrupoDeUsuarioController();
+
+	
+	
+// Invocando o método de Listagem
+	
+echo $GrupoDeUsuarioController->listarGrupoDeUsuario();
+	
+// Fechando a conexão
+
+$con->closeConnection();
+
+?>
+  </section>
+  <footer>RODAPE</footer>
+</section>
+</body>
+</html>
