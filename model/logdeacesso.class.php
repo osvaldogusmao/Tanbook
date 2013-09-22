38f0422820c0be02a124cc618f9e8a65ff3c4469
IvanSimionato <ivansimionato@rocketmail.com>
2013-09-22 11:56:20 -0300

    LogDeAcesso model incluido

---
+++ model/logdeacesso.class.php
@@ -0,0 +1,51 @@
+<?php 
+
+/*
+	Descrição do arquivo
+	@autor - Ivan Simionato
+	@data de criação - 22/09/2013
+	@arquivo - logdeacesso.class.php
+
+*/
+
+
+class LogDeAcesso{
+	
+	function __construct(){
+		
+	}
+
+	private $id;
+	private $usuario_id;
+	private $dataAcesso;
+
+	public function getId(){
+	    return $this->id;
+	}
+	 
+	public function setId($id){
+		$this->id = $id;
+	}
+
+	public function getUsuario_id(){
+		return $this->usuario_id;
+	}
+
+	public function setUsuario_id($usuario_id){
+		$this->usuario_id = $usuario_id;
+	}
+
+	public function getDataAcesso(){
+	    return $this->dataAcesso;
+	}
+	 
+	public function setDataAcesso($dataAcesso){
+		$this->dataAcesso = $dataAcesso;
+	}
+
+
+}
+
+
+
+ ?>
