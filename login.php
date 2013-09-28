11282a9cb5f1fa7bcc393143121f18fa1933f920
Lukas Robeto <lukas.roberto@yahoo.com.br>
2013-09-28 20:57:20 -0300

    Criada View de Login

---
+++ login.php
@@ -0,0 +1,29 @@
+<!DOCTYPE html>
+<html>
+<head>
+	<meta charset="utf-8">
+	<title>Projeto Tanbook</title>
+	<link href="css/style.css" rel="stylesheet" type="text/css">
+</head>
+<body>
+<section id="wrapperLogin">
+	<section id="content">
+		<form action="">
+			<h1>Login Tanbook</h1>
+			<div>
+				<input type="text" placeholder="Usuário" required id="username" />
+			</div>
+			<div>
+				<input type="password" placeholder="Senha" required id="password" />
+			</div>
+			<div>
+				<input type="submit" value="Entrar" />
+				<a href="#">Esqueceu sua senha?</a>
+				<a href="#">Registre-se</a>
+			</div>
+		</form><!-- form -->
+	</section><!-- content -->
+</div><!-- container -->
+</section>
+</body>
+</html>
