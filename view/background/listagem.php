1a6a297e00df40197f73f66071f34e896e4011ef
João Evangelista <jaoevangelista@users.noreply.github.com>
2013-10-21 01:17:03 -0200

    Update listagem.php

--- view/background/listagem.php
+++ view/background/listagem.php
@@ -1,10 +1,15 @@
 <?php
+/*
+Joao Pedro Evangelista 
+20/10/2013
+Listagem de imagem
 
-//include
+*/
+
 include_once '../../controller/background.controller.class.php';
 include_once '../../model/background.class.php';
 
-//login
+
 session_start();
 if (!isset($_SESSION['id'])) {
   header("Location: ../../view/login/login.php");
@@ -14,7 +19,7 @@ if ((isset($_GET['action'])) && ($_GET['action'] == 'logout' )) {
   $loginController->logout();
   header("Location: ../../view/login/login.php");
 }
-//instancia
+
 $controller = new BackgroundController();
 
 
@@ -25,6 +30,10 @@ $controller = new BackgroundController();
 <html lang="en">
 <head>
  <meta charset="UTF-8">
+ <link href="../../css/style.css" rel="stylesheet" type="text/css">
+ <link href="../../css/bootstrap.css" rel="stylesheet" type="text/css">
+ <link href="../../css/validation.css" rel="stylesheet" type="text/css">
+ <link href="../../css/bootstrap-responsive.css" rel="stylesheet" type="text/css">
  <title>Backgrounds</title>
 </head>
 <body>
@@ -40,32 +49,34 @@ $controller = new BackgroundController();
           <th>Status</th>
         </tr>
       </thead>
-      <tbody>
-        <?php
-        $res = $controller->load($_SESSION['grupoDeUsuario_id'], "grupo_id");
 
-        if (isset($res)){
-         while ($campos = mysql_fetch_array($res)):
-          ?>
-        <tr>
-          <td>
-            <img src="<?php $campos['path'] ?>" alt="background">
-          </td>
-          <td>
-            <?php echo $campos['descricao'] ?>
-          </td>
-          <td>
-            <?php echo $campos['status'] ?>
-          </td>
-          <td>
-            <a href="deletar.php?id=<?php echo $campos['id']?>">Deletar</a>
-          </td>
-          <a href="editar.php?id=<?php echo $campos['id']?>">Alterar</a>
-        </tr>
-      <?php  endwhile; }    ?>
-  </tbody>
-</table>
-</div>
+
+      <?php
+      $res = $controller->loadObject($_SESSION['grupoDeUsuario_id'], "grupo_id");
+
+      if (!empty($res)){
+       while ($campos = mysql_fetch_array($res));
+       ?>
+       <tr>
+        <td>
+          <img src="<?php $campos['path'] ?>" alt="background">
+        </td>
+        <td>
+          <?php echo $campos['descricao'] ?>
+        </td>
+        <td>
+          <?php echo $campos['status'] ?>
+        </td>
+        <td>
+          <a href="deletar.php?id=<?php echo $campos['id']?>">Deletar</a>
+        </td>
+        <a href="editar.php?id=<?php echo $campos['id']?>">Alterar</a>
+      </tr>
+      <?php }else{
+        die("Sem dados");
+      } ?>
+    </table>
+  </div>
 </div>
 </body>
 </html>
