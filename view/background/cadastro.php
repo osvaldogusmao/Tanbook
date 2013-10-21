6d34b73df09cb1d041396aed4227b4525d263f84
João Evangelista <jaoevangelista@users.noreply.github.com>
2013-10-21 01:13:39 -0200

    Update cadastro.php

--- view/background/cadastro.php
+++ view/background/cadastro.php
@@ -20,51 +20,49 @@ if ((isset($_GET['action'])) && ($_GET['action'] == 'logout' )) {
 }
 
 
-$dirPath ="../../backgrounds/" ;
-
-function setChmod()
-{
-    chmod($dirPath, 0777);
-}
-function check()
-{
-    $image_info = getimagesize($_FILES["file_field_name"]["tmp_name"]);
-    $image_width = $image_info[0];
-    $image_height = $image_info[1];
-
-    if(($_FILES["file"]["size"] > 256000)
-       || ($_FILES["file"]["type"] !="image/png")
-       || ($_FILES["file"]["type"] !="image/jpg"))
-    {
-        echo "Arquivo Invalido";
-        if (file_exists($dirPath . $_FILES["file"]["name"]))
-        {
-            echo "Arquivo já existe";
-
-            if ($image_height>800 || $image_width >600) {
-                echo "Arquivo possui dimensoes alem das permitidas";
-            }
-        }
-        return false;
-    }else{
-        $this->setChmod();
-        move_uploaded_file($_FILES["file"]["tmp_name"],
-                           $dirPath . $_FILES["file"]["name"]);
-    }
-}
-
-
 $controller = new BackgroundController();
 $model = new Background();
+$dirPath = "../../backgrounds/";
 
-if(isset($_POST['submit'])){
+if(isset($_POST['btn'])){
 
-$model->setPath($dirPath.$_FILES["file"]["name"]);
-$model->setGrupo_id($_SESSION["grupoDeUsuario_id"]);
-$model->setDescricao($_POST["descricao"]);
-$model->getStatus($_POST["status"]);
+  $image_info = getimagesize($_FILES["file"]["tmp_name"]);
+  $image_width = $image_info[0];
+  $image_height = $image_info[1];
+  $extensionAllowed = array("gif", "jpeg", "jpg", "png");
+  $temp = explode(".", $_FILES["file"]["name"]);
+  $extension = end($temp);
 
-$controller->save($model);
+  if(($image_height>800
+     || $image_width >600)
+    && (($_FILES["file"]["type"] == "image/gif")
+        || ($_FILES["file"]["type"] == "image/jpeg")
+        || ($_FILES["file"]["type"] == "image/jpg")
+        || ($_FILES["file"]["type"] == "image/pjpeg")
+        || ($_FILES["file"]["type"] == "image/x-png")
+        || ($_FILES["file"]["type"] == "image/png"))
+    && in_array($extension, $extensionAllowed)
+    && file_exists($dirPath . $_FILES["file"]["name"])
+    )
+  {
+
+    echo "Arquivo Invalido!";
+  }else{
+    chmod($dirPath, 0777);
+    move_uploaded_file($_FILES["file"]["tmp_name"],
+                       $dirPath . $_FILES["file"]["name"]);
+
+
+    $model->setPath($dirPath.$_FILES["file"]["name"]);
+    $model->setGrupo_id($_SESSION["grupoDeUsuario_id"]);
+    $model->setDescricao($_POST["descricao"]);
+    $model->getStatus($_POST["status"]);
+
+    $controller->save($model);
+
+  }
+
+
 }
 
 ?>
@@ -74,23 +72,27 @@ $controller->save($model);
 <html lang="en">
 <head>
  <meta charset="UTF-8">
- <title>Document</title>
+ <link href="../../css/style.css" rel="stylesheet" type="text/css">
+ <link href="../../css/bootstrap.css" rel="stylesheet">
+ <link href="../../css/validation.css" rel="stylesheet">
+ <link href="../../css/bootstrap-responsive.css" rel="stylesheet">
+ <title>Upload Imagem</title>
 </head>
 <body>
  <div id="container">
-     <div id="form">
-         <form action="cadastro.php" method="POST">
+   <div id="form">
+     <form action="cadastro.php" method="POST" enctype="multipart/form-data">
 
-            <label for="status">Status</label>
-            <input type="text" id="status" name="status">
-            <label for="descricao">Descrição</label>
-            <input type="text" id="descricao" name="descricao">
-            <label for="file">Arquivo:</label>
-            <input type="file" id="file" name="file">
-            <input type="submit" value="Enviar" name="submit">
+      <label for="status">Status</label>
+      <input type="text" id="status" name="status">
+      <label for="descricao">Descrição</label>
+      <input type="text" id="descricao" name="descricao">
+      <label for="file">Arquivo:</label>
+      <input type="file" id="file" name="file">
+      <input type="submit" value="Enviar" name="btn">
 
-        </form>
-    </div>
+    </form>
+  </div>
 </div>
 </body>
 </html>
